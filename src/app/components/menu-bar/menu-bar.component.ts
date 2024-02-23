6e8bc5ede61d1eb9f0886c0758d08f2416d315ca
Pamella oliveira <pamchinha125@gmail.com>
2024-02-22 22:18:22 -0300

    content created

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css']
+
 })
 export class MenuBarComponent implements OnInit {
 
